4168b343f01e8e1a2cc369d25b7e297537a493e3
nmarin <nmarin@gestorinc.com>
2017-12-22 11:13:46 -0500

    Se Modifica la clase TipoUsuarioEnum

--- admin-console-ejb/src/main/java/com/gestor/bots/admin/console/enums/TipoUsuarioEnum.java
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/enums/TipoUsuarioEnum.java
@@ -1,14 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * GESTOR
+ * Sistema: CONSOLA ADMINISTRACION BOT
+ * Creado: 22-dic-2017 - 09:43:30
+ * 
+ * Los contenidos de este archivo son propiedad intelectual de Gestorinc S.A.
+ * y se encuentran protegidos por la licencia: "GESTOR FIDUCIA/FONDOS G5".
+ * 
+ * Usted puede encontrar una copia de esta licencia en: 
+ *   http://www.gestorinc.com
+ * 
+ * Copyright 2015-2018 Gestorinc S.A. Todos los derechos reservados.
  */
 package com.gestor.bots.admin.console.enums;
 
 /**
- *
- * @author Hendrix
+ *Define los valores posibles para Tipos de usuarios.
+ * @author Gestor
  */
 public enum TipoUsuarioEnum {
+    
+    /** Suscripcion */
+    INT("Interno"),
+    /** Pregunta y Respuesta*/
+    EXT("Externo");
+    
+    private final String etiqueta;
+    
+    private TipoUsuarioEnum(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
     
 }
