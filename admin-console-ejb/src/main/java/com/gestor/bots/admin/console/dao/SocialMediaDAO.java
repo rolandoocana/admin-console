4dfc0f2ad48636d3a56f802e14ffb07e4d933d2b
sjerez <sjerez@gestorinc.com>
2018-01-03 10:56:05 -0500

    creacion de enumeracion

---
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/dao/SocialMediaDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gestor.bots.admin.console.dao;
+
+import com.gestor.bots.admin.console.model.SocialMedia;
+import com.persist.common.dao.DefaultGenericDAOImple;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Gestor_1
+ */
+@LocalBean
+@Stateless
+public class SocialMediaDAO extends DefaultGenericDAOImple<SocialMedia, String>{
+    public SocialMediaDAO() {
+        super(SocialMedia.class);
+    }
+}
