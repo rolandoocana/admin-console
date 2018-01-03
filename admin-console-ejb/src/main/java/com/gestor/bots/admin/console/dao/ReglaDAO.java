9a515688f6a58ea2ee1e3c7e8fa7e1cd25016d8d
rocana <rocana@gestorinc.com>
2018-01-03 10:52:30 -0500

    Clase Regla DAO

---
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/dao/ReglaDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gestor.bots.admin.console.dao;
+
+import com.gestor.bots.admin.console.model.Regla;
+import com.persist.common.dao.DefaultGenericDAOImple;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Gestor
+ */
+@LocalBean
+@Stateless
+public class ReglaDAO extends DefaultGenericDAOImple<Regla, String> {
+    public ReglaDAO() {
+        super(Regla.class);
+    }
+    
+}
