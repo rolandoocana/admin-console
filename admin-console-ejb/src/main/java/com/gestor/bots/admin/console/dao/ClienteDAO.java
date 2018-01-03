464c2ea8d228a216a2a1e39cad7375b88811ac27
hcoral <hcoral@gestorinc.com>
2018-01-03 10:41:54 -0500

    Agregadas clases genericas DAO

---
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/dao/ClienteDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gestor.bots.admin.console.dao;
+
+import com.gestor.bots.admin.console.model.Cliente;
+import com.persist.common.dao.DefaultGenericDAOImple;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Hendrix
+ */
+@LocalBean
+@Stateless
+public class ClienteDAO extends DefaultGenericDAOImple<Cliente, String> {
+    
+    public ClienteDAO() {
+        super(Cliente.class);
+    }
+}
