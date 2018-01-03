8284921891d27d0bcbd7dcf726a0786d28e7105e
sjerez <sjerez@gestorinc.com>
2018-01-03 10:51:37 -0500

    creacion de enumeracion

---
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/dao/CorpusDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gestor.bots.admin.console.dao;
+
+import com.gestor.bots.admin.console.model.Corpus;
+import com.persist.common.dao.DefaultGenericDAOImple;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Gestor_1
+ */
+@LocalBean
+@Stateless
+public class CorpusDAO extends DefaultGenericDAOImple<Corpus, String>{
+     
+    public CorpusDAO() {
+        super(Corpus.class);
+    } 
+}
