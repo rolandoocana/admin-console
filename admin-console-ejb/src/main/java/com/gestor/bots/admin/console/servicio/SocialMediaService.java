7f078d39152b7e2e7f75b259ec1d888f5aa80c4a
rocana <rocana@gestorinc.com>
2018-01-04 10:44:34 -0500

    Clase SocialMedia DAO

--- admin-console-ejb/src/main/java/com/gestor/bots/admin/console/servicio/SocialMediaService.java
+++ admin-console-ejb/src/main/java/com/gestor/bots/admin/console/servicio/SocialMediaService.java
@@ -7,6 +7,7 @@ package com.gestor.bots.admin.console.servicio;
 
 import com.gestor.bots.admin.console.dao.SocialMediaDAO;
 import com.gestor.bots.admin.console.model.SocialMedia;
+import com.gestor.bots.admin.exception.CreacionException;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -31,9 +32,26 @@ public class SocialMediaService {
         try {
             this.socialMediaDAO.insert(socialMedia);
         } catch (Exception e) {
-            throw new CreacionException("ERR001", e.getMessage() ,e);
+            throw new CreacionException("ERR100", e.getMessage() ,e);
         }
     }
     
+    public void modificar(SocialMedia socialMedia){
+        try{
+            this.socialMediaDAO.update(socialMedia);
+        }catch (Exception e){
+            throw new ModificacionException("ERR200", e.getMessage(), e);
+        }
+    }
+    
+    public void eliminar(SocialMedia socialMedia){
+        try{
+            this.socialMediaDAO.remove(socialMedia);
+        }catch (Exception e){
+            throw new EliminacionException("ERR300", e.getMessage(), e);
+        }
+    }
+    
+  
     
 }
